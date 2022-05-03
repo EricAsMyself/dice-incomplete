0ec475e019ae8f88d5dcb6b9e13a741e9b2faed6
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2022-05-02 19:28:01 -0600

    added begining introductions to class and function

--- dice/game/die.py
+++ dice/game/die.py
@@ -2,11 +2,13 @@ from csv import DictReader
 import random
 
 class Die:
+    #creatsed an instance of a Die
     def __init__(self):
         self.points = 0
         self.value = 0
 
     def roll(self):
+        #Rolls the die and gets value. It also calculates the score.
         self.value = random.randint(1,6)
         if self.value == 1:
             self.points += 100
